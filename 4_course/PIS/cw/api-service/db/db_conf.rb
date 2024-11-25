a1759a1fd0921060004d8ec1b7c1469f7ac12a46
RikersMax <larionov01996@gmail.com>
2024-11-25 21:20:07 +0500

    remove cw/add web lb_6

--- 4_course/PIS/cw/api-service/db/db_conf.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'module_db.rb'
-
-path = Dir.pwd.gsub(/\/api-services*./, '/api-services/storage/db.sqlite3')
-
-DBApp.config do |conf| 
-  conf.adapter = 'sqlite3'
-  conf.path_db = path
-  conf.create_version_table
-end
